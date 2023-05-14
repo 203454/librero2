f760bf2d76147563e9e94de9ae82f1028fbcbd33
Ciro Diaz <cirow310@outlook.es>
2023-05-13 20:52:41 -0600

    c

--- libro/views.py
+++ libro/views.py
@@ -14,37 +14,37 @@ class LibrosView(View):
         return super().dispatch(request, *args, **kwargs)
         
     def get(self, request, id=0):
-    if id > 0:
-        libros = list(Libro.objects.filter(id=id).values())
-        if len(libros) > 0:
-            libro = libros[0]
-            datos = {'message': "Success", 'libros': libro}
-        else:
-            datos = {'message': "Libro not found"}
-        return JsonResponse(datos)
-    else:
-        status_param = request.GET.get('status', None)
-        if status_param is not None:
-            if status_param.lower() == 'true':
-                libros = list(Libro.objects.filter(status=True).values())
-            elif status_param.lower() == 'false':
-                libros = list(Libro.objects.filter(status=False).values())
-            else:
-                datos = {'message': "Invalid status parameter"}
-                return JsonResponse(datos)
-
+        if id > 0:
+            libros = list(Libro.objects.filter(id=id).values())
             if len(libros) > 0:
-                datos = {'message': "Success", 'libros': libros}
+                libro = libros[0]
+                datos = {'message': "Success", 'libros': libro}
             else:
-                datos = {'message': "Libros not found"}
+                datos = {'message': "Libro not found"}
+            return JsonResponse(datos)
         else:
-            libros = list(Libro.objects.values())
-            if len(libros) > 0:
-                datos = {'message': "Success", 'libros': libros}
-            else:
-                datos = {'message': "Libros not found"}
+            status_param = request.GET.get('status', None)
+            if status_param is not None:
+                if status_param.lower() == 'true':
+                    libros = list(Libro.objects.filter(status=True).values())
+                elif status_param.lower() == 'false':
+                    libros = list(Libro.objects.filter(status=False).values())
+                else:
+                    datos = {'message': "Invalid status parameter"}
+                    return JsonResponse(datos)
 
-        return JsonResponse(datos)
+                if len(libros) > 0:
+                    datos = {'message': "Success", 'libros': libros}
+                else:
+                    datos = {'message': "Libros not found"}
+            else:
+                libros = list(Libro.objects.values())
+                if len(libros) > 0:
+                    datos = {'message': "Success", 'libros': libros}
+                else:
+                    datos = {'message': "Libros not found"}
+
+            return JsonResponse(datos)
 
 
 
